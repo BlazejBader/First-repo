8e6468952b6f521bcefdbb91a2d377f09b922a46
BlazejBader <BlazejBader@wp.pl>
2025-03-09 17:36:04 +0100

    js

---
+++ lekcje/ćwiczenia js/script.js
@@ -0,0 +1,6 @@
+const students = [
+  { name: "Bartek", age: 12, gender: "male", number: 5 },
+  { name: "Kasia", age: 11, gender: "female", number: 3 },
+  { name: "Ania", age: 13, gender: "female", number: 4 },
+];
+console.log(students[1].name);
